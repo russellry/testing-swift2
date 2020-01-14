3f1e94e56a4453ee86c4519c7b8325b82c23d68d
Russell Ong <digivicer@hotmail.com>
2020-01-14 11:18:59 -0800

    Initial Commit

---
+++ testing2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing2
+//
+//  Created by Russell Ong on 1/14/20.
+//  Copyright © 2020 Russell Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testing2Tests/testing2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  testing2Tests.swift
+//  testing2Tests
+//
+//  Created by Russell Ong on 1/14/20.
+//  Copyright © 2020 Russell Ong. All rights reserved.
+//
+
+import XCTest
+@testable import testing2
+
+class testing2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
